4bdb985aa9d40d95b42780c1b5a316adccb40d80
mj0107 <alswns1285@gmail.com>
2025-07-12 16:55:04 +0900

    feat: 헤더 프로필 아이콘 구현 #6 - 프로필 아이콘 클릭 시 드롭다운 메뉴 표시   - 내 포스티파이, 임시글, 읽기 목록, 설정, 로그아웃

---
+++ src/constants/header-dropdown.ts
@@ -0,0 +1,22 @@
+export const HEADER_DROPDOWN_MENU_ITEMS = [
+  {
+    label: "내 포스티파이",
+    href: "/:id/posts",
+  },
+  {
+    label: "임시 글",
+    href: "/saves",
+  },
+  {
+    label: "읽기 목록",
+    href: "/lists",
+  },
+  {
+    label: "설정",
+    href: "/setting",
+  },
+  {
+    label: "로그아웃",
+    href: "/logout",
+  },
+];
