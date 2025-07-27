34458a2296fce1fa2b93f683baab808f778afb4f
mj0107 <alswns1285@gmail.com>
2025-07-27 23:19:08 +0900

    feat: authStore 구현 #22 - zustand 이용하여 토큰 포함 user 관련 정보 session storage에 전역으로 관리

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,4 +1,4 @@
-import { User } from "@/types/api/user";
+import { User } from "@/types/user";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
