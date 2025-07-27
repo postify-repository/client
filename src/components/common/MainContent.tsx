7f8b75d319e3ee75688f4ccab91675e241b2de07
mj0107 <alswns1285@gmail.com>
2025-07-27 22:44:01 +0900

    design: MainContent 마진 수정

--- src/components/common/MainContent.tsx
+++ src/components/common/MainContent.tsx
@@ -4,7 +4,7 @@ interface MainContentProps {
 
 export default function MainContent({ children }: MainContentProps) {
   return (
-    <div className="flex flex-col gap-4 mt-30 mx-auto max-w-screen-md">
+    <div className="flex flex-col gap-4 mt-20 md:mt-30 mx-auto max-w-screen-md">
       {children}
     </div>
   );
