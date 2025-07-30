d718e605d8c26547348abe304e91f4da325dd66c
mj0107 <alswns1285@gmail.com>
2025-07-30 23:19:08 +0900

    feat: 에러 메시지 분기 처리 및 문구 변경 #22

--- src/hooks/auth/useLoginForm.ts
+++ src/hooks/auth/useLoginForm.ts
@@ -25,10 +25,17 @@ export const useLoginForm = () => {
       },
       onError: (error) => {
         if (error instanceof AxiosError) {
-          const errorMessage =
-            error.response?.status === axios.HttpStatusCode.Unauthorized
-              ? error.response?.data.message
-              : "알 수 없는 오류가 발생했습니다.";
+          const status = error.response?.status;
+          let errorMessage = "";
+
+          if (status === axios.HttpStatusCode.Unauthorized) {
+            errorMessage = error.response?.data.message;
+          } else if (status === axios.HttpStatusCode.InternalServerError) {
+            errorMessage =
+              "서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.";
+          } else {
+            errorMessage = "알 수 없는 오류가 발생했습니다.";
+          }
 
           form.setError("root.server", { message: errorMessage });
         }
